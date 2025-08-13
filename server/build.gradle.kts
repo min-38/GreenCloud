143ba3369554c030dba019542b61bc767a7a8c79
min-38 <mindino03@gmail.com>
2025-08-13 22:00:53 +0900

    [Client] [Feature] 서버 Health Check API 호출 및 상태 표시 #3 완료
    https://github.com/min-38/GreenCloud/issues/3

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "com.greencloud"
@@ -20,6 +21,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
