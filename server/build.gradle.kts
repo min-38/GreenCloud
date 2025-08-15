923e1faa63e8c52fb5f80a481aa0fd18c252725f
min-38 <mindino03@gmail.com>
2025-08-15 23:04:27 +0900

    [Feature] Neon PostgreSQL 연결 & users 스키마 설계 #5
    https://github.com/min-38/GreenCloud/issues/5#issuecomment-3191580688

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,16 +21,19 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
